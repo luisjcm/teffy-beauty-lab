0ddd73bf7a3f587238ff132cd5afc033da7baaaf
luisjcm <ljcm47@gmail.com>
2026-06-20 19:22:27 -0400

    build: generar carpeta docs para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/teffy-beauty-lab/', // CRÍTICO: El nombre exacto de tu repositorio
+  build: {
+    outDir: 'docs', // Vite creará la carpeta 'docs' para GitHub Pages
+    emptyOutDir: true // Limpia la carpeta antes de cada nuevo build
+  }
 });
