6ccc776afe457a17831efcd358e4d7ab8d2b8180
luisjcm <ljcm47@gmail.com>
2026-06-20 20:32:51 -0400

    fix: base path para subdominio

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/teffy-beauty-lab/', // CRÍTICO: El nombre exacto de tu repositorio
+  base: './', // CRÍTICO: El nombre exacto de tu repositorio
   build: {
     outDir: 'docs', // Vite creará la carpeta 'docs' para GitHub Pages
     emptyOutDir: true // Limpia la carpeta antes de cada nuevo build
